1b86f835af75abb49b4042ea609fbe582ea79db3
yxyfer <mathieu.rivier@epita.fr>
2025-04-27 14:26:05 +0200

    (delete) reset repo

--- batches/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  output: 'export',
-  distDir: '../docs',
-  assetPrefix: 'https://xhecfounders.com/'
-};
-
-module.exports = nextConfig;
